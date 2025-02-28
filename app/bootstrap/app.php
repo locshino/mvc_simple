acdece00e2093b4c28d8aef46ea871f872b465e9
Shino303 <locshino123@gmail.com>
2025-02-28 22:34:12 +0700

    Update app.php

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -1,11 +1,12 @@
 <?php
 require_once __DIR__.'/../../vendor/autoload.php';
-require_once __DIR__.'/../helpers/register.php';
+require_once __DIR__.'/../Core/helpers/register.php';
 
 // Khởi tạo session nếu chưa có
 if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
+
 // Load .env
 loadEnv(); 
 
