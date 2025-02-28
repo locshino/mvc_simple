5aed00eef4ac815bef7f7241d6d07220dd7faf4b
Shino303 <locshino123@gmail.com>
2025-02-28 21:57:55 +0700

    rename helpers/index to helpers/register

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__.'/../../vendor/autoload.php';
-require_once __DIR__.'/../helpers/index.php';
+require_once __DIR__.'/../helpers/register.php';
 
 // Khởi tạo session nếu chưa có
 if (session_status() === PHP_SESSION_NONE) {
